393244044ee1a7b9fce9abf47ababf7c7106efac
lutfi420 <80556716+lutfi420@users.noreply.github.com>
2021-03-14 02:55:51 +0700

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} Afk dulu{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                '{} DUAR MEMEK AK DATANG!', '{} YUHU BALIK LAGI!', '{} ADA DISINI!',
+                '{} DUAR KONTOL AK HADIR!', '{} AK DATANG!', '{} HADIR HADIR HADIR!',
+                'Anak kontol! {}', 'Dimana coba {}?\nCiluk ba!'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
